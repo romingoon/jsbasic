7d175c6fe65a4381ca2989edaeda31928d3010d3
romingoon <romingoon@gmail.com>
2022-07-23 08:09:27 +0900

    220721 try this - arr.Reduce 고차함수 콜백함수 표현

--- arrReduce.js
+++ arrReduce.js
@@ -1,14 +1,17 @@
 
-const sum = (arr, func, iv) => {
+const reduce = (arr, func, iv) => {
     let total = iv ? iv : arr[0]
 
-    for(let [idx, el] of arr.entries()){
+    for(let [idx] of arr.entries()){
         if(idx !== arr.length-1)
         total = func(total, arr[idx + 1]) ;
     }
     return total;
 };
 
-const a = [1, 2, 3];
-console.log(sum(a, (acc, crr) => acc + crr));
+console.log(reduce([1,2,3], (a, b) => a + b, 0));
+console.log(reduce([1,2,3,4,5], (a,b) => a +b));
+console.log(reduce([1,2,3,4,5], (a,b) => a*b, 1));
+console.log(reduce([2,2,2], (a,b)=> a*b));
+
 
