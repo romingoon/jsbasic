2c543e010243b72e8ba5e483a580f8be3b0ef8ff
LAPTOP-2QMUBS4B\youm0 <romingoon@gmail.com>
2022-07-28 21:50:23 +0900

    220728 Array Try This 추가/리팩토링

---
+++ arrTryRefac.js
@@ -0,0 +1,60 @@
+const range = (start, end, step = start > end ? -1 : 1) => {
+    // if ((end !== start && step > 0) || ((end - start) * step < 0))
+    if ((start > end && step > 0) || (start < end && step < 0))
+        return [];
+
+    const tmp = start;
+    if (start === 0) end = start;
+    end ?? (end = start > 0 ? tmp : -1, start = start < 0 ? tmp : 1);
+
+    const arr = [];
+    for (let i = start; step > 0 ? i <= end : i >= end; i += step) {
+        arr.push(i);
+    }
+
+    return arr;
+};
+
+console.time('total');
+// console.time('1st');
+
+// console.log('---------------------------------');
+console.log('range(1, 10, 1) =', range(1, 10, 1));  // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log('range(1, 10, 2) =', range(1, 10, 2));  // [1, 3, 5, 7, 9]
+
+console.log('range(1, 10) = ', range(1, 10));     // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log('range(10, 1) = ', range(10, 1));     // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+console.log('range(1, 10, 0) =', range(1, 10, 0));  // []
+console.log('range(-1, -10, -1) =', range(-1, -10, -1));  // [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
+
+console.log('range(0, 20, -1) =', range(0, 20, -1));  // []
+console.log('range(10, 10, 1) = ', range(10, 10, 1));     // [ 10 ]
+
+console.log('range(10, 1, -1) =', range(10, 1, -1)); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+console.log('range(10, 1, -2) = ', range(10, 1, -2)); // [ 10, 8, 6, 4, 2 ]
+console.log('range(-10, -5, 1) = ', range(-10, -5, 1)); // [ -10, -9, -8, -7, -6, -5 ]
+
+console.log('range(20, 0, 5) = ', range(20, 0, 5)); // []
+
+console.log('range(5) = ', range(5));  // [1, 2, 3, 4, 5] 
+console.log('range(-5) = ', range(-5));  // [ ? ] 
+console.log('range(5, 5) = ', range(5, 5));  // [ 5 ] 
+// console.log('range(5, 5, 0) = ', range(5, 5, 0));  // [ 5 ] 
+console.log('range(5, 5, -1) = ', range(5, 5, -1));  // [ 5 ] 
+console.log('range(5, 1, 1) = ', range(5, 1, 1));  // [ ] 
+console.log('range(1, 5, -1) = ', range(1, 5, -1));  // [ ] 
+console.log('range(1, 5, 6) = ', range(1, 5, 6));  // [ 1 ] 
+
+
+console.log('range(0) = ', range(0));  // [ 0 ] 
+console.log('range(0, 0) = ', range(0, 0));  // [ 0 ] 
+console.log('range(0, 0, 5) = ', range(0, 0, 5));  // [ 0 ] 
+console.log('range(0, 3) = ', range(0, 3));  // [ 1 ] 
+console.log('range(0, -1) = ', range(0, -1));  // [ 1 ] 
+console.log('range(0, -3) = ', range(0, -3));  // [ 1 ] 
+console.log('range(-3, 0) = ', range(-3, 0));  // [ 1 ] 
+console.log('range(2, 1, -5) = ', range(2, 1, -5));  // [ 2 ] 
+
+// console.timeEnd('1st');
+console.timeEnd('total');
