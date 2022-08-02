0d51c971dae45d140580e927d087b23919d78bae
LAPTOP-2QMUBS4B\youm0 <romingoon@gmail.com>
2022-08-02 18:41:42 +0900

    220802 closure

--- closures.js
+++ closures.js
@@ -1,3 +1,39 @@
+const x = 1;
+function outer() {
+  const x = 10;
+
+  function inner() {
+    console.log(x);
+  }
+  return inner;
+}
+
+const innerFn = outer();
+innerFn();
+
+
+
+
+// let cnt = 0;
+// function cntPlus() {
+//   cnt = cnt + 1;
+// }
+
+// console.log(cnt);
+// cntPlus();
+// console.log(cnt);
+
+// // 1억개의 코드
+
+// cnt = 1000;
+
+// // // 1억개의 코드
+// cntPlus();
+// console.log(cnt);
+
+
+
+
 function closure() {
   let cnt = 0;
   function cntPlus() {
@@ -12,6 +48,7 @@ function closure() {
   return {
     cntPlus,
     printCnt,
+    setCnt,
   };
 }
 
@@ -22,18 +59,11 @@ console.log(cntClo);
 cntClo.printCnt();
 cntClo.cntPlus();
 cntClo.printCnt();
+
+// console.log(cnt);
+
 cntClo.setCnt(100);
+cntClo.printCnt();
+cntClo.cntPlus();
+cntClo.printCnt();
 
-// console.log(cnt);
-
-// console.log(cnt);
-// cntPlus();
-// console.log(cnt);
-
-// // 1억개의 코드
-
-// cnt = 1000;
-
-// // 1억개의 코드
-// cntPlus();
-// console.log(cnt);
