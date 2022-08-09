e945923d943cbefa77e82eea6f9206d8d027954a
romingoon <romingoon@gmail.com>
2022-08-09 18:21:58 +0900

    220809 splice 구현

---
+++ symbol.js
@@ -0,0 +1,6 @@
+// const mySymbol = new Symbol();
+//TypeError: Symbol is not a constructor
+// symbol 함수는 new 연산자 와함께 호출하지 않음
+
+const mySymbol = Symbol();
+console.log(mySymbol); // Symbol();
--- tt_reduce.js
+++ tt_reduce.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, 4, 5];
 
-const powReduce = arr.reduce()
+const s = [];
+const pow = arr.reduce((s, idx) => (s = arr.push(arr[idx] ** 2)));
 
-}
-
+console.log(pow);
 // 결과 => [1, 8, 27, 64, 125];
